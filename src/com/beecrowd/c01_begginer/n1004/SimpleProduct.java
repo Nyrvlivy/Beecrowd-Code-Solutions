63e219e4d17a87e88e0bc0b313a3312d073fc2e6
Nyrvlivy <nyrvlivy@gmail.com>
2024-04-04 14:25:04 -0300

    feat(exercise): solve beecrowd exercise 1004

---
+++ src/com/beecrowd/c01_begginer/n1004/SimpleProduct.java
@@ -0,0 +1,18 @@
+package com.beecrowd.c01_begginer.n1004;
+
+import java.util.Scanner;
+
+public class SimpleProduct {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        int PROD = A * B;
+
+        System.out.printf("PROD = %d%n", PROD);
+
+    }
+}
