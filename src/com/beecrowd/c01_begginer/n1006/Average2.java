bc03205e36be357e87aa0cc016e8a7c62b996970
Nyrvlivy <nyrvlivy@gmail.com>
2024-04-04 17:13:11 -0300

    feat(exercise): solve beecrowd exercise 1006

---
+++ src/com/beecrowd/c01_begginer/n1006/Average2.java
@@ -0,0 +1,19 @@
+package com.beecrowd.c01_begginer.n1006;
+
+import java.util.Scanner;
+
+public class Average2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double A = scanner.nextDouble();
+        double B = scanner.nextDouble();
+        double C = scanner.nextDouble();
+        scanner.close();
+
+        double MEDIA = ((A * 2) + (B * 3) + (C * 5)) / 10;
+
+        System.out.printf("MEDIA = %.1f%n", MEDIA);
+    }
+}
