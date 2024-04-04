de4350add57790793a424a886ddd8546807b3747
Nyrvlivy <nyrvlivy@gmail.com>
2024-04-04 14:04:59 -0300

    feat(exercise): solve beecrowd exercise 1003

---
+++ src/com/beecrowd/c01_begginer/n1003/SimpleSum.java
@@ -0,0 +1,19 @@
+package com.beecrowd.c01_begginer.n1003;
+
+import java.util.Scanner;
+
+public class SimpleSum {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        int SOMA = A + B;
+
+        System.out.printf("SOMA = %d%n", SOMA);
+
+    }
+}
