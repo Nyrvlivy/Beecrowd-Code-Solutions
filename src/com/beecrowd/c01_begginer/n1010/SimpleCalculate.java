410bb8bc1096863f474b93b7ca8fd7c32d69aed2
Nyrvlivy <nyrvlivy@gmail.com>
2024-04-07 00:17:54 -0300

    feat(exercise): solve beecrowd exercise 1010

---
+++ src/com/beecrowd/c01_begginer/n1010/SimpleCalculate.java
@@ -0,0 +1,26 @@
+package com.beecrowd.c01_begginer.n1010;
+
+import java.util.Scanner;
+
+public class SimpleCalculate {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String data = scanner.useDelimiter("\\A").next();
+        scanner.close();
+
+        String[] products = data.split("\\s*\\n\\s*");
+
+        double total = 0;
+
+        for (String product : products) {
+            String[] field = product.split(" ");
+            int quantity = Integer.parseInt(field[1]);
+            double price = Double.parseDouble(field[2]);
+
+            total += quantity * price;
+        }
+
+        System.out.printf("VALOR A PAGAR: R$ %.2f\n", total);
+    }
+}
