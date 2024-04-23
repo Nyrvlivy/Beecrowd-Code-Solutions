7a5b6cf4175749c4e766b57cf12ad3ec85bb177b
nyrvlivy <dev.brunamassi@gmail.com>
2024-04-23 07:36:51 -0300

    feat(exercise): solve beecrowd exercise 1002

---
+++ Python/c01_begginer/n1002/area_of_a_circle.py
@@ -0,0 +1,12 @@
+PI = 3.14159
+radius = float(input())
+area = PI * (radius ** 2)
+
+print(f'A={area:.4f}')
+
+# if pi is not defined in the problem, you can use the math library
+# import math
+#
+# radius = float(input())
+# area = math.pi * (radius ** 2)
+# print(f'A={area:.4f}')
