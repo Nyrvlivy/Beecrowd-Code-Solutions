6466e7e1b59617ea892a3114014ff908df345f4c
nyrvlivy <dev.brunamassi@gmail.com>
2024-04-28 07:33:16 -0300

    feat(exercise): solve beecrowd exercise 1008

---
+++ Python/c01_begginer/n1008/salary.py
@@ -0,0 +1,7 @@
+employeeNumber = int(input())
+workedHours = int(input())
+hourlyRate = float(input())
+
+salary = workedHours * hourlyRate
+
+print(f'NUMBER = {employeeNumber}\nSALARY = U$ {salary:.2f}')
